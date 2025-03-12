130c4d91d55e799d4dbc815f612514d7aba07498
Pratik Dhumne <45841102+Pratikdh45@users.noreply.github.com>
2025-03-12 20:21:05 +0530

    Create for-loop.sh

---
+++ for-loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# # Loop from 1 to 5
+for i in {1..5}
+do
+  echo "Iteration: $i"
+done
+
+
+# for name in Alice, Bob, Charlie
+# do
+#   echo "Hello, $name!"
+# done
+
+
+# for file in *.txt
+# do
+#   echo "Processing $file"
+# done
